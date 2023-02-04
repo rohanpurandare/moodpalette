81d45a8e9dd5e8173a6e615d316a12df13746532
Aayushi Neema <neema.aayushi@gmail.com>
2023-02-04 16:03:54 -0500

    created cron schedule and pulled from api

--- moodpalette/src/App.js
+++ moodpalette/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import GetDailyQuote from "./dailyQuote";
 
 function App() {
   return (
     <div className="App">
-      
+      <GetDailyQuote></GetDailyQuote>
     </div>
   );
 }
