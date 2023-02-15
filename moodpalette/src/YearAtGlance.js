5b45f160d15c5f9c534872c6b778b0f5f3faa215
Rohan Purandare <rpuranda@purdue.edu>
2023-02-14 23:43:59 -0500

    added number of days and removed non-year days from year at a glance

--- moodpalette/src/YearAtGlance.js
+++ moodpalette/src/YearAtGlance.js
@@ -1,16 +1,21 @@
 import { useEffect, useRef } from "react";
+import data from './calendar-data-source.json'
 
 function App() {
     const canvasRef = useRef();
   
     const drawRectangle = () => {
         const context = canvasRef.current.getContext("2d");
-        
+        const validDays = data.calendarDataSource.percentData;
         for (let i = 0; i < 7; i++) {
-            for (let j = 0; j < 54; j++) {
+            for (let j = 0; j < 53; j++) {
                 var n = (Math.random() * 0xfffff * 1000000).toString(16);
+                if (validDays[j][i] === null) {
+                    n = (0xffffff).toString(16);
+                }
+                console.log(n)
                 context.fillStyle = "#" + n.slice(0, 6);
-                context.fillRect(j*18.5, i*18.5, 17, 17);
+                context.fillRect(j*25, i*25, 24, 24);
             }
         }
     };
@@ -24,7 +29,7 @@ function App() {
             <canvas
             ref={canvasRef}
             height={400}
-            width={1000}
+            width={1500}
             />
         </div>
     );
