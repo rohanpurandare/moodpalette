91847929b3dcb1056f5e8eca802f1b8f8e515a96
Aayushi Neema <neema.aayushi@gmail.com>
2023-02-21 14:34:37 -0500

    fixed navbar hamburger and right alignment

--- moodpalette/src/navbar/index.jsx
+++ moodpalette/src/navbar/index.jsx
@@ -1,17 +1,23 @@
 import styled from "styled-components";
 import NavLinks from "./navLinks";
 import { useMediaQuery } from "react-responsive";
-import MobileNavLinks from "./mobileNavLinks";
 import Accessibility from "./accessibility";
 import Logo from "./logo";
+import React, { useState } from "react";
+import MenuToggle from "./menuToggle";
+
+const NavBarOuterContainer = styled.div`
+`;
 
 const NavBarContainer = styled.div`
     width: 100%;
-    height: 160px;
+    height: 100px;
     box-shadow: 0 1px 3px rgba(15, 15, 15, 0.13);
     display: flex;
     align-items: center;
     padding: 0 1.5em;
+    margin-top: 60px;
+    box-sizing: border-box;
 `;
 
 const LeftSection = styled.div`
@@ -29,6 +35,51 @@ const RightSection = styled.div`
     display: flex;
 `;
 
+const NavButtonContainer = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+const MobileNavContainer = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const LinksWrapper = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  height: 100%;
+  list-style: none;
+  background-color: #fff;
+  width: 100%;
+  flex-direction: column;
+  top: 65px;
+  left: 0;
+`;
+
+const LinkItem = styled.li`
+  width: 100%;
+  padding: 0 1.1em;
+  color: #222;
+  font-weight: 500;
+  font-size: 16px;
+  display: flex;
+  margin-bottom: 10px;
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  color: inherit;
+  font-size: inherit;
+`;
+
+const Marginer = styled.div`
+  height: 2em;
+`;
 const DeviceSize = {
     mobile: 768,
     tablet: 992,
@@ -38,16 +89,31 @@ const DeviceSize = {
 
 export default function NavBar(props) {
     const isMobile = useMediaQuery({maxWidth: DeviceSize.mobile});
-    return <NavBarContainer>
-        <LeftSection>
-            <Logo />
-        </LeftSection>
-        <MiddleSection>
-            {!isMobile && <NavLinks></NavLinks>}
-            {isMobile && <MobileNavLinks></MobileNavLinks>}
-        </MiddleSection>
-        <RightSection>
-            {!isMobile && <Accessibility />}
-        </RightSection>
-    </NavBarContainer>
+    const [isOpen, setOpen] = useState(false);
+    return <NavBarOuterContainer>
+        <NavBarContainer>
+            <LeftSection>
+                <Logo />
+            </LeftSection>
+            <MiddleSection>
+                {!isMobile && <NavLinks></NavLinks>}
+            </MiddleSection>
+            <RightSection>
+                {!isMobile && <Accessibility />}
+                {isMobile && <NavButtonContainer>
+                    <MenuToggle isOpen={isOpen} toggle={() => setOpen(!isOpen)} />
+                </NavButtonContainer>}
+            </RightSection>
+        </NavBarContainer>
+        {isMobile && <MobileNavContainer>
+            {isOpen && <LinksWrapper>
+                    <LinkItem><Link href="#">Calendar View</Link></LinkItem>
+                    <LinkItem><Link href="#">Habit Tracker</Link></LinkItem>
+                    <LinkItem><Link href="#">Moo Pal Shop</Link></LinkItem>
+                    <LinkItem><Link href="#">Account</Link></LinkItem>
+                    <Marginer />
+                    <Accessibility />
+                </LinksWrapper>}
+        </MobileNavContainer>}
+    </NavBarOuterContainer>
 }
